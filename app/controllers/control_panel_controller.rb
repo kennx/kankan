db030dade49b361e2e27b173fd681f412676f26e
kennx <zhangxjian@me.com>
2012-09-21 17:23:01 +0800

    解决一张photo能有多个相同的tag问题.

--- app/controllers/control_panel_controller.rb
+++ app/controllers/control_panel_controller.rb
@@ -7,15 +7,15 @@ class ControlPanelController < ApplicationController
     erb(:'photos/add')
   end
 
-  get '/cpanel/edit_user/?' do
-    @user = User.find_by(:id => session[:user_id])
-    erb(:'users/edit')
-  end
+  # get '/cpanel/edit_user/?' do
+  #   @user = User.find_by(:id => session[:user_id])
+  #   erb(:'users/edit')
+  # end
 
   post '/user/:id/photo/create/?' do
     @user = User.find(params[:id])
     tags = params[:tags]
-    tag_list = tags.split(" ")
+    tag_list = tags.downcase.split(" ").uniq
     NORMAL_DIR = "./public/uploader/attachment/" + dir_format_to_date + "/"
     THUMB_DIR = "./public/uploader/attachment/" + dir_format_to_date + "/_thumb/"
     if params[:file] && (tmpfile = params[:file][:tempfile]) && (filename = params[:file][:filename])
@@ -33,9 +33,9 @@ class ControlPanelController < ApplicationController
       normal_url = "/uploader/attachment/" + dir_format_to_date + "/" + rename
       thumbnail_url = "/uploader/attachment/" + dir_format_to_date + "/_thumb/" + rename
       if crop_medium_image(normal_path) && crop_image_fixed_size(thumb_path, 320, 220)
-        create_photo = @user.photos.build(:photo_url => "#{normal_url}", :thumbnail_url => "#{thumbnail_url}", :statuse => params[:description])
+        create_photo = @user.photos.create(:photo_url => "#{normal_url}", :thumbnail_url => "#{thumbnail_url}", :statuse => params[:description])
         tag_list.each {|t| create_photo.tags << Tag.new(:name => t)}
-        if create_photo.save
+        if create_photo.save!
           flash[:notice] = "发布成功"
           redirect back
         else
@@ -46,8 +46,8 @@ class ControlPanelController < ApplicationController
     end
   end
 
-  put '/cpanel/user/:id/?' do
-    # PUT
-  end
+  # put '/cpanel/user/:id/?' do
+  #   # PUT
+  # end
 
 end
